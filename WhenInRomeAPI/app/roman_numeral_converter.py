6eec28bfc3d7384dd7cd72e7de7aef5878e1112b
Adrien Cogny <6886475+ac4649@users.noreply.github.com>
2023-01-23 16:41:19 -0500

    removed some print statements created all tests for roman_numeral class created some tests from roman numeral converter class

--- WhenInRomeAPI/app/roman_numeral_converter.py
+++ WhenInRomeAPI/app/roman_numeral_converter.py
@@ -21,7 +21,7 @@ class RomanNumeralConverter:
         # 2. We check that non-repeatable numerals are not present more than once
         # 3. We subtract subtractable numerals from the sum if we are allowed to
         for i in range(len(self.numeral)):
-            print(previous_numerals)
+
             # We create the roman numeral for the current character
             try:
                 cur_numeral = RomanNumeral(self.numeral[i])
@@ -69,7 +69,7 @@ class RomanNumeralConverter:
         return returned_number
 
     def convert_to_roman(self):
-        print("converting the number to roman")
+
         if not self.number:
             raise Exception("No Number set")
 
--- WhenInRomeAPI/tests/test_roman_numeral.py
+++ WhenInRomeAPI/tests/test_roman_numeral.py
@@ -10,10 +10,39 @@ class TestRomanNumeral:
         assert RomanNumeral("I").value == 1
         assert RomanNumeral("I").repeatable == True
         assert RomanNumeral("I").subtractable == True
-        # "I": [ 1, True, True ],
-        # "V": [ 5, False, False ],
-        # "X": [ 10, True, True ],
-        # "L": [ 50, False, False ],
-        # "C": [ 100, True, True ],
-        # "D": [ 500, False, False ],
-        # "M": [ 1000, True, False ]
+
+        assert RomanNumeral("V").numeral == "V"
+        assert RomanNumeral("V").value == 5
+        assert RomanNumeral("V").repeatable == False
+        assert RomanNumeral("V").subtractable == False
+        
+        assert RomanNumeral("X").numeral == "X"
+        assert RomanNumeral("X").value == 10
+        assert RomanNumeral("X").repeatable == True
+        assert RomanNumeral("X").subtractable == True
+
+        assert RomanNumeral("L").numeral == "L"
+        assert RomanNumeral("L").value == 50
+        assert RomanNumeral("L").repeatable == False
+        assert RomanNumeral("L").subtractable == False
+
+        assert RomanNumeral("C").numeral == "C"
+        assert RomanNumeral("C").value == 100
+        assert RomanNumeral("C").repeatable == True
+        assert RomanNumeral("C").subtractable == True
+
+        assert RomanNumeral("D").numeral == "D"
+        assert RomanNumeral("D").value == 500
+        assert RomanNumeral("D").repeatable == False
+        assert RomanNumeral("D").subtractable == False
+
+        assert RomanNumeral("M").numeral == "M"
+        assert RomanNumeral("M").value == 1000
+        assert RomanNumeral("M").repeatable == True
+        assert RomanNumeral("M").subtractable == False
+
+
+    def test_unknown_numeral(self):
+
+        with pytest.raises(Exception, match="Invalid Numeral"):
+            RomanNumeral("P")
