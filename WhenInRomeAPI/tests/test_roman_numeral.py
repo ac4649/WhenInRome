39edd56c79832d3886a21eaa88528c886bfca8ab
Adrien Cogny <6886475+ac4649@users.noreply.github.com>
2023-01-24 13:15:17 -0500

    additional tests for roman numeral class

--- WhenInRomeAPI/tests/test_roman_numeral.py
+++ WhenInRomeAPI/tests/test_roman_numeral.py
@@ -3,6 +3,32 @@ import pytest
 
 class TestRomanNumeral:
 
+    def test_is_valid_numeral_valid(self):
+        assert RomanNumeral.is_valid_numeral("I") == True
+        assert RomanNumeral.is_valid_numeral("V") == True
+        assert RomanNumeral.is_valid_numeral("X") == True
+        assert RomanNumeral.is_valid_numeral("L") == True
+        assert RomanNumeral.is_valid_numeral("C") == True
+        assert RomanNumeral.is_valid_numeral("D") == True
+        assert RomanNumeral.is_valid_numeral("M") == True
+        assert RomanNumeral.is_valid_numeral("_I") == True
+        assert RomanNumeral.is_valid_numeral("_V") == True
+        assert RomanNumeral.is_valid_numeral("_X") == True
+
+    def test_is_valid_numeral_invalid(self):
+        assert RomanNumeral.is_valid_numeral("O") == False
+        assert RomanNumeral.is_valid_numeral("K") == False
+
+    def test_is_valid_subtraction(self):
+        assert RomanNumeral.is_valid_subtraction("I", "V") == True
+        assert RomanNumeral.is_valid_subtraction("I", "X") == True
+        assert RomanNumeral.is_valid_subtraction("X", "L") == True
+        assert RomanNumeral.is_valid_subtraction("X", "C") == True
+        assert RomanNumeral.is_valid_subtraction("C", "D") == True
+        assert RomanNumeral.is_valid_subtraction("C", "M") == True
+        assert RomanNumeral.is_valid_subtraction("_I", "_V") == True
+        assert RomanNumeral.is_valid_subtraction("_I", "_X") == True
+
     def test_known_numerals(self):
         # Here we test the values of the known roman numerals
 
@@ -46,3 +72,17 @@ class TestRomanNumeral:
 
         with pytest.raises(Exception, match="Invalid Numeral"):
             RomanNumeral("P")
+
+    def test_can_be_subtracted_from(self):
+        valid_subtraction = RomanNumeral("X")
+        assert valid_subtraction.canBeSubtractedFrom() == True
+        valid_subtraction.times_found += 2
+        assert valid_subtraction.canBeSubtractedFrom() == True
+
+
+        invalid_subtraction = RomanNumeral("V")
+        assert invalid_subtraction.canBeSubtractedFrom() == True
+
+        # If we found the invalid subtraction once we
+        invalid_subtraction.times_found += 1
+        assert invalid_subtraction.canBeSubtractedFrom() == False
